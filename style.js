13cb5567b8771bcb34bc73b4b07039468565e94d
whoisBugsbunny <whoisBugsbunny>
2023-09-28 03:29:56 +0530

    ui2

--- style.js
+++ style.js
@@ -4,6 +4,16 @@ const navigation = document.querySelector('.navigation-left');
 const courcesCard = document.querySelectorAll('.courcesCard');
 const courcebox = document.querySelector('.courcebox');
 const courceboxback = document.querySelector('.courceboxback');
+const useremail = document.querySelector('#useremail');
+const password = document.querySelector('#password');
+const loginbtn = document.querySelector('#loginbtn');
+const loginpage = document.querySelector('#loginpage');
+const aboutus = document.querySelector('#aboutus');
+const aboutbox = document.querySelector('.aboutbox');
+const closeBottomBtn = document.querySelector('#close-bottom-btn');
+const profileBtn = document.querySelector('#profilebtn');
+const profilebox = document.querySelector('.profilebox');
+const profileboxback = document.querySelector('.profileboxback');
 // const nav = document.querySelector('#main-nav');
 
 burger.addEventListener('click', () => {
@@ -24,4 +34,33 @@ courcesCard.forEach(cource => {
 });
 courceboxback.addEventListener('click', () => {
     courcebox.classList.remove('go_up');
+});
+
+loginbtn.addEventListener('click', () => {
+    if (useremail.value == '' || password.value == '') {
+        alert('Please fill all the fields');
+    } else if (useremail.value == 'tester@gmail.com' || password.value == 'tester') {
+        // loginpage.classList.add('d-none');
+        loginpage.classList.add('logincontainer_up');
+
+    } else {
+        alert('Please enter correct credentials');
+    }
+});
+
+aboutus.addEventListener('click', () => {
+    navigation.classList.toggle('h-left');
+    aboutbox.classList.toggle('aboutboxhide');
+});
+
+closeBottomBtn.addEventListener('click', () => {
+    aboutbox.classList.toggle('aboutboxhide');
+});
+
+profileBtn.addEventListener('click', () => {
+    profilebox.classList.toggle('profileboxhide');
+});
+
+profileboxback.addEventListener('click', () => {
+    profilebox.classList.toggle('profileboxhide');
 });
